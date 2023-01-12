85e26f4b8603b97adecd8cfa7a80335c2a5a2625
nokori <69152061+ucelery@users.noreply.github.com>
2023-01-13 02:10:43 +0800

    Update Testing Grounds

--- backend/db_test.py
+++ backend/db_test.py
@@ -5,21 +5,23 @@ from schemas.user import User
 from schemas.schedule import Schedule
 import traceback
 
-print(django.get_version())
-
 creds_path = 'credentials.json'
 f = open(creds_path)
 creds = json.load(f)
 
 mongoHandler = MongoHandler(creds, "DoLaLash")
 
-users = mongoHandler.insert_user({
-        "username" : "beepBoopBit",
-        "password" : "watermelon",
-        "f_name" : "Beep",
-        "l_name" : "Boop",
-        "email" : "beepBoopBit+DoLaLash@gmail.com",
-        "mobile" : "07373737373"
-    })
+# user_id = mongoHandler.insert_user({
+#         "username" : "beepBoopBit",
+#         "password" : "watermelon",
+#         "f_name" : "Beep",
+#         "l_name" : "Boop",
+#         "email" : "beepBoopBit+DoLaLash@gmail.com",
+#         "mobile" : "07373737373"
+#     })
 
-print(mongoHandler.delete_user("63c0406ffaec984550c1dec6"))
+# print(mongoHandler.find_user({ "username": "beepBoopBit"}))
+
+# mongoHandler.delete_user(user_id)
+
+# print(mongoHandler.find_user({ "id": user_id}))
