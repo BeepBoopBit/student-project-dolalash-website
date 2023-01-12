3094449a9325f96aaddcfe9b284ab84da38960ea
luisbanez <luismbanez@gmail.com>
2023-01-13 00:25:58 +0800

    add test html pages and their respective views

--- virtual_environment/dolalash_website/members/urls.py
+++ virtual_environment/dolalash_website/members/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('products/', views.products, name='products'),
+    path('profile/', views.profile, name='profile'),
+    path('about/', views.about, name='about'),
 ]
--- virtual_environment/dolalash_website/members/views.py
+++ virtual_environment/dolalash_website/members/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def members(request):
-    template = loader.get_template('testing.html')
-    return HttpResponse(template.render())
+
+def generate_html_view(path_to_html: str):    
+    def view(request):
+        template = loader.get_template(path_to_html)
+        return HttpResponse(template.render())
+    return view
+
+home = generate_html_view("home.html")
+services = generate_html_view("services.html")
+products = generate_html_view("products.html")
+profile = generate_html_view("profile.html")
+about = generate_html_view("about.html")
